ff6fc4822fc0cac5ed3be6a6af9e74dd0a708721
Spirit5736 <kozhevnikovi@mail.ru>
2021-06-13 22:50:29 +0700

    added error text output

--- Program.cs
+++ Program.cs
@@ -1,16 +1,25 @@
 ﻿using System;
+using System.IO;
 
 namespace Unique_words_on_the_page
 {
     class Program
-    {
+    {    
         static void Main(string[] args)
-        {
-            Console.WriteLine("Введите адрес страницы:");
-            Console.Write("http://");
-            string link = "http://" + Console.ReadLine();
-            HTMLQueryProcessing hTMLQueryProcessing = new HTMLQueryProcessing();
-            hTMLQueryProcessing.TextProcessing(WebReader.DownloadString(link));
+        {           
+            try
+            {
+                Console.WriteLine("Введите адрес страницы:");
+                Console.Write("http://");
+                string link = "http://" + Console.ReadLine();
+                HTMLQueryProcessing hTMLQueryProcessing = new HTMLQueryProcessing();
+                hTMLQueryProcessing.TextProcessing(WebReader.DownloadString(link));
+            }
+
+            catch
+            {
+                Console.WriteLine("Не корректно введен адрес, работа приложена будет завершена.");
+            }
         }
     }
 }
